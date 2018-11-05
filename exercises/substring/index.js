3549d741b8c78874abab586ca7adfb6f1f00fbbc
DMW9400 <dmw9400@gmail.com>
2018-11-04 21:55:40 -0500

    eliminated need for arr and sort in substr

--- exercises/substring/index.js
+++ exercises/substring/index.js
@@ -6,33 +6,32 @@ let var1 = 'abcsaebsefetrz'
 
 function uniqString(string_arg){
 
-    let subStr = []
+      let subStr = []
 
-    for (let i=0;i<string_arg.length;i++){
-      let char = string_arg[i]
-      let uniqArr = []
-      let uniqStr = char
-      uniqArr.push(char)
-      let remainStr = string_arg.slice(i+1,string_arg.length)
+      for (let i=0;i<string_arg.length;i++){
+        let char = string_arg[i]
+        let uniqArr = []
+        let uniqStr = char
+        let comparStr = char
+        uniqArr.push(char)
+        let remainStr = string_arg.slice(i+1,string_arg.length)
 
-      for (let z=0;z<remainStr.length;z++){
-        let subChar = remainStr[z]
-        if (!uniqArr.includes(subChar)){
-            uniqArr.push(subChar)
-            uniqStr = uniqStr + subChar
-        } else {
-          subStr.push(uniqStr)
-          uniqStr = ''
-          uniqArr = []
+        for (let z=0;z<remainStr.length;z++){
+          let subChar = remainStr[z]
+          if (!uniqArr.includes(subChar)){
+              uniqArr.push(subChar)
+              comparStr = comparStr + subChar
+              if (comparStr.length > uniqStr.length){
+                uniqStr = comparStr
+              }
+          } else {
+            subStr.push(uniqStr)
+            uniqStr = ''
+            uniqArr = []
+          }
         }
       }
-    }
-
-    subStr.sort(function(a,b){
-      return b.length - a.length
-    })
-
-    return subStr[0]
+      return subStr[0]
 }
 
 uniqString(var1)
