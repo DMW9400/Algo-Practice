ec57269675ec7556dac4e27e814a1215581fb1ec
DMW9400 <dmw9400@gmail.com>
2018-11-08 13:54:25 -0500

    solved and drastically improved answer

--- exercises/expanded_form_numbers.js
+++ exercises/expanded_form_numbers.js
@@ -1,35 +1,19 @@
 function expandedForm(num) {
-
+  let subNumArr = []
   let numStr = num.toString()
-  let numlength = numStr.length
-  let returnStr = ''
 
-  for (let n=0; n < numlength; n++){
-    let zeroCount = numlength - n - 1
+  for (let n = 0; n < numStr.length; n++){
+    let zeroCount = numStr.length - n - 1
     let x = 0
     let subNum = numStr[n]
-    let remStr = numStr.slice(n,numStr.length)
 
-    if (n < numlength - 1 && numStr[n] !== '0' && remStr.split('').includes(!0)){
-
-     while (x < zeroCount) {
-      subNum = subNum + '0'
-      x++
+    if(numStr[n] !== '0'){
+       while (x < zeroCount) {
+        subNum = subNum + '0'
+        x++
       }
-
-      returnStr = returnStr + subNum
-    }  else if (n < numlength - 1 && numStr[n] !== '0'){
-
-     while (x < zeroCount) {
-      subNum = subNum + '0'
-      x++
-      }
-
-      returnStr = returnStr + subNum + ' + '
-    }
-    else if (n == numlength - 1 && numStr[n]  !== '0') {
-      returnStr = returnStr + subNum
+      subNumArr.push(subNum)
     }
   }
-  return returnStr
+  return subNumArr.join(' + ')
 }
